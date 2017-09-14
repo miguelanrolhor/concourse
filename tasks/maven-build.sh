5fd2a2345244fc9eb5da7e1f058d9324213bc17b
Miguel Angel Roldán <miguel-angel.roldan@audatexdatos.es>
2017-09-14 14:53:05 +0200

    tests

---
+++ tasks/maven-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+./pipeline/tasks/generate-settings.sh
+
+cd source-code || echo "missing input resource: source-code"
+
+echo "Using MAVEN_OPTS: ${MAVEN_OPTS}"
+
+mvn verify ${MAVEN_ARGS}
